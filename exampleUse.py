5e6969660ed42a425e5c5da17538c18c6b2b5c40
AnnaLindeberg <anna@familjenlindeberg.com>
2024-11-04 15:51:38 +0100

    Output files instead of print to terminal

--- exampleUse.py
+++ exampleUse.py
@@ -1,7 +1,5 @@
 import networkx as nx
 import phylox.newick_parser
-from phylox import DiNetwork
-from phylox.constants import LENGTH_ATTR
 from phylox.newick_parser import dinetwork_to_extended_newick
 from networkx.drawing.nx_pydot import write_dot
 from lca_LCA_relevant import *
@@ -16,8 +14,8 @@ def relabelPhyloX(label):
 def main():
     # Example usage:
     # Parse the Viola network, provided in (extended) Newick format
-
-    with open("viola.newick") as newickFile:
+    input_file_name = "violaN"
+    with open(input_file_name) as newickFile:
         newickStr = newickFile.readline().strip()
         G = phylox.newick_parser.extended_newick_to_dinetwork(newickStr)
         G = nx.relabel_nodes(G, relabelPhyloX)
@@ -33,9 +31,10 @@ def main():
 
 
     # Display the edges of the LCA-relevant DAG
-    print("Edges of the LCA-relevant DAG:", list(LCA_rel_G.edges))
-    # Display the edges of the lca-relevant DAG
-    print("Edges of the lca-relevant DAG:", list(lca_rel_G.edges))
+    # print("Edges of the LCA-relevant DAG:", list(LCA_rel_G.edges))
+    # print(LCA_rel_newick)
+    # # Display the edges of the lca-relevant DAG
+    # print("Edges of the lca-relevant DAG:", list(lca_rel_G.edges))
 
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", DeprecationWarning)
@@ -43,15 +42,24 @@ def main():
         # Unfortunately, pydot is not actively maintained but still works here
         # It is probably better to use Pygraphviz, but larger dependencies to install
         nxGraphs = [G, LCA_rel_G, lca_rel_G]
-        fileNames = ["viola-org", "viola-LCArel", "viola-lowercase-lcarel"]
-        for graph, fileName in zip(nxGraphs, fileNames):
+        fileEndings = ["-orig", "-LCArel", "-lowercase-lcarel"]
+        for graph, fileEnding in zip(nxGraphs, fileEndings):
+            newFileName = input_file_name + fileEnding
+            as_newick = dinetwork_to_extended_newick(graph, simple=True)
+            with open(newFileName + '.newick', 'w') as newFile:
+                newFile.write(as_newick)
+            
+            with open(newFileName + '-edges.txt', 'w') as newFile:
+                newFile.write(str(graph.edges))
+    
             pydotGraph = nx.nx_pydot.to_pydot(graph)
-            pydotGraph.write_png(fileName + '.png')
+            pydotGraph.write_png(newFileName + '.png')
+            print(f"Output written into {newFileName}.png, {newFileName}-edges.txt and {newFileName}.newick")        
         
         # If you DO have pygraphviz installed and working, the following can be used
 #        dotGraph = nx.nx_agraph.to_agraph(graph)
 #        fileType = '.png'
-#        dotGraph.draw(fileName + fileType, format=fileType[1:], prog='dot' )
+#        dotGraph.draw(fileEnding + fileType, format=fileType[1:], prog='dot' )
 
 if __name__ == '__main__':
     main()
